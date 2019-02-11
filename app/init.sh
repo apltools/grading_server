ee21f715faba692a468fea4dac7d322cfbecbd24
jelleas <jelle.van.assema@gmail.com>
2019-02-11 20:05:44 +0100

    fix 999

---
+++ app/init.sh
@@ -0,0 +1,10 @@
+DOCKER_SOCKET=/var/run/docker.sock
+DOCKER_GROUP=docker_host
+REGULAR_USER=app_user
+
+DOCKER_GID=$(stat -c '%g' ${DOCKER_SOCKET})
+addgroup -g ${DOCKER_GID} ${DOCKER_GROUP}
+addgroup ${REGULAR_USER} ${DOCKER_GROUP}
+chown -R app_user:app_group /app
+
+su - app_user -c 'sh /app/cont.sh'
