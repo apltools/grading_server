13a46b179848ca3ca75bffa29c28ddc15c80eec3
jelleas <jelle.van.assema@gmail.com>
2019-02-11 14:24:07 +0100

    use https

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
+if [ ! -d ./app/certs ]; then
+  mkdir ./app/certs
+  openssl req -x509 -newkey rsa:4086 \
+    -subj "/C=NL/ST=XXXX/L=XXXX/O=UvA/CN=localhost" \
+    -keyout "./app/certs/priv.pem" \
+    -out "./app/certs/pub.pem" \
+    -days 3650 -nodes -sha256
+fi
+
 docker-compose -f docker-compose.yml -f docker-compose-check.yml build
