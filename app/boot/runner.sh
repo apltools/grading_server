dd4e158f76898b8fa4bfc5e6eb91f130f758a7b6
jelleas <jelle.van.assema@gmail.com>
2022-08-30 14:43:46 +0200

    version fixes

--- app/boot/runner.sh
+++ app/boot/runner.sh
@@ -1,3 +1,3 @@
 cd /app
 PATH=$PATH:/home/app_user/.local/bin:/home/app_user/.local/lib/python3.7/site-packages
-uwsgi --master --https 0.0.0.0:5000,certs/pub.pem,certs/priv.pem --wsgi-file app.py --callable app --processes 4 --threads 2
+uwsgi --master --https 0.0.0.0:5000,certs/pub.pem,certs/priv.pem --wsgi-file app.py --callable app --processes 4 --threads 2 --logto /tmp/scheduler.log
